1ce2be3960410be610a51cf7a85522bddc896e79
James Naadjie <james.naadjie@gmail.com>
2016-08-14 15:14:03 -0400

    add item routing specs

--- spec/routing/items_routing_spec.rb
+++ spec/routing/items_routing_spec.rb
@@ -1,47 +1,31 @@
-require "rails_helper"
+require 'rails_helper'
 
 module Api
 	module V1
 		RSpec.describe ItemsController, type: :routing do
-			describe "routing" do
-				it "routes to #index" do
-					pending 'Not yet implemented'
-					expect(:get => "/items").to route_to("items#index")
+			describe 'routing' do
+				it 'routes to #index' do
+					expect(:get => '/api/v1/menu/items').to route_to('api/v1/items#index')
 				end
 
-				it "routes to #new" do
-					pending 'Not yet implemented'
-					expect(:get => "/items/new").to route_to("items#new")
+				it 'routes to #show' do
+					expect(:get => '/api/v1/menu/items/1').to route_to('api/v1/items#show', :id => '1')
 				end
 
-				it "routes to #show" do
-					pending 'Not yet implemented'
-					expect(:get => "/items/1").to route_to("items#show", :id => "1")
+				it 'routes to #create' do
+					expect(:post => '/api/v1/menu/items').to route_to('api/v1/items#create')
 				end
 
-				it "routes to #edit" do
-					pending 'Not yet implemented'
-					expect(:get => "/items/1/edit").to route_to("items#edit", :id => "1")
+				it 'routes to #update via PUT' do
+					expect(:put => '/api/v1/menu/items/1').to route_to('api/v1/items#update', :id => '1')
 				end
 
-				it "routes to #create" do
-					pending 'Not yet implemented'
-					expect(:post => "/items").to route_to("items#create")
+				it 'routes to #update via PATCH' do
+					expect(:patch => '/api/v1/menu/items/1').to route_to('api/v1/items#update', :id => '1')
 				end
 
-				it "routes to #update via PUT" do
-					pending 'Not yet implemented'
-					expect(:put => "/items/1").to route_to("items#update", :id => "1")
-				end
-
-				it "routes to #update via PATCH" do
-					pending 'Not yet implemented'
-					expect(:patch => "/items/1").to route_to("items#update", :id => "1")
-				end
-
-				it "routes to #destroy" do
-					pending 'Not yet implemented'
-					expect(:delete => "/items/1").to route_to("items#destroy", :id => "1")
+				it 'routes to #destroy' do
+					expect(:delete => '/api/v1/menu/items/1').to route_to('api/v1/items#destroy', :id => '1')
 				end
 
 			end
