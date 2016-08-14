619c537039badae84d40aad8ba273304902db7a6
James Naadjie <james.naadjie@gmail.com>
2016-08-14 12:18:21 -0400

    passing spec

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
-
-#I need to create a new order
-#i need to send a post to order_controller#create with item_id
-#might need customer id
-#api/v1/orders/:customer_id/:item_id
-#
-# def create
-# 	order = Order.new
-#
-# end
-
 RSpec.describe 'Orders', type: :request do
 	describe 'GET Orders /api/v1/customers/:id/orders' do
 		it 'responds with 200 status' do
+			create(:user)
 			get '/api/v1/customers/1/orders'
 			expect(response).to have_http_status(200)
+		end
+
+		it 'returns orders for specified customer' do
+			user_one = create(:user, email: 'user@foo.com')
+			user_two = create(:user, email: 'user2@foo.com')
+			user_one.orders.create!
+			user_two.orders.create
+
+			get '/api/v1/customers/1/orders'
+
+			body = JSON.parse(response.body)[0]
+			expect(body['id']).to eq(1)
+			expect(body['user_id']).to eq(1)
 		end
 	end
 end
