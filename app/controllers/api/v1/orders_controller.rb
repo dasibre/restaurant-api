e090628df9b593187a0422ac547475681236a55f
James Naadjie <james.naadjie@gmail.com>
2016-08-14 12:20:35 -0400

    add index action body

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,7 +1,11 @@
 module Api
 	module V1
 		class OrdersController < ApplicationController
+
 			def index
+				@customer = User.find(params[:customer_id])
+				@orders = @customer.orders
+				render json: @orders
 			end
 
 			def show
